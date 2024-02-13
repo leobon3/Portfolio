ad48da984c6333471e805bc3d8a8014740b23248
leobon3 <leobon33@gmail.com>
2024-02-13 21:30:01 +0800

    asda

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
-    <h1>INDEX PORTFOLIO</h1>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/skins/color-1.css">
 </head>
 <body>
-    <h1>sadas</h1>
+    
+
+    <!-- JS FILES -->
+    <script src="js/script.js"></script>
 </body>
 </html>
